646e3376bf4549724ed3cb9d2dcb27e7dbdf3e2a
hatchedland <mail.rajanydv@gmail.com>
2025-05-22 12:51:21 +0530

    fix: init.sh

--- init.sh
+++ init.sh
@@ -2,12 +2,12 @@
 
 # Create directories not tracked by Git
 mkdir -p data
-mkdir -p keys/service-accounts
+mkdir -p secrets/service-accounts
 mkdir -p exports
 mkdir -p upload-doc
 
 # Add directories to .gitignore
 echo "data/" >> .gitignore
-echo "keys/service-accounts/" >> .gitignore
+echo "secrets/service-accounts/" >> .gitignore
 echo "exports/" >> .gitignore
 echo "upload-doc/" >> .gitignore
